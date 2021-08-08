03971794ad0cfbad2cae217ccdfd0c18e1537935
wulu473 <lukas@wulu.at>
2021-08-08 15:59:53 +0000

    Refactor epsilon computation

--- prv_accountant/discrete_privacy_random_variable.py
+++ prv_accountant/discrete_privacy_random_variable.py
@@ -15,3 +15,21 @@ class DiscretePrivacyRandomVariable:
     def __len__(self) -> int:
         assert len(self.pmf) == len(self.domain)
         return len(self.pmf)
+
+    def compute_epsilon(self, delta, delta_error, epsilon_error):
+        t = self.domain.ts()
+        p = self.pmf
+        d1 = np.flip(np.flip(p).cumsum())
+        d2 = np.flip(np.flip(p*np.exp(-t)).cumsum())
+        ndelta = np.exp(t) * d2-d1
+
+        def find_epsilon(delta_target):
+            i = np.searchsorted(ndelta, -delta_target, side='left')
+            if i <= 0:
+                raise RuntimeError("Cannot compute epsilon")
+            return np.log((d1[i-1]-delta_target)/d2[i-1])
+       
+        eps_upper = find_epsilon(delta - delta_error) + epsilon_error
+        eps_lower = find_epsilon(delta + delta_error) - epsilon_error
+        eps_estimate = find_epsilon(delta)
+        return eps_lower, eps_estimate, eps_upper
