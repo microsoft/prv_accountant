bc17dd04da5d8c9f07e1c677ec8deb22d1ecdc4d
Lukas Wutschitz <lukas.wutschitz@microsoft.com>
2021-10-20 13:35:12 +0000

    Whitespace edits

--- prv_accountant/privacy_random_variables.py
+++ prv_accountant/privacy_random_variables.py
@@ -143,103 +143,101 @@ def _compute_log_a(q, sigma, alpha):
 
 
 def _log_comb(n, k):
-  return (special.gammaln(n + 1) - special.gammaln(k + 1) -
-          special.gammaln(n - k + 1))
+    return (special.gammaln(n + 1) - special.gammaln(k + 1) -
+            special.gammaln(n - k + 1))
 
 
 def _compute_log_a_int(q, sigma, alpha):
-  """Compute log(A_alpha) for integer alpha. 0 < q < 1."""
-  assert isinstance(alpha, six.integer_types)
+    """Compute log(A_alpha) for integer alpha. 0 < q < 1."""
+    assert isinstance(alpha, six.integer_types)
 
-  # Initialize with 0 in the log space.
-  log_a = -np.inf
+    # Initialize with 0 in the log space.
+    log_a = -np.inf
 
-  for i in range(alpha + 1):
-    log_coef_i = (
-        _log_comb(alpha, i) + i * math.log(q) + (alpha - i) * math.log(1 - q))
+    for i in range(alpha + 1):
+        log_coef_i = _log_comb(alpha, i) + i * math.log(q) + (alpha - i) * math.log(1 - q)
 
-    s = log_coef_i + (i * i - i) / (2 * (sigma**2))
-    log_a = _log_add(log_a, s)
+        s = log_coef_i + (i * i - i) / (2 * (sigma**2))
+        log_a = _log_add(log_a, s)
 
-  return float(log_a)
+    return float(log_a)
 
 
 def _log_add(logx, logy):
-  """Add two numbers in the log space."""
-  a, b = min(logx, logy), max(logx, logy)
-  if a == -np.inf:  # adding 0
-    return b
-  # Use exp(a) + exp(b) = (exp(a - b) + 1) * exp(b)
-  return math.log1p(math.exp(a - b)) + b  # log1p(x) = log(x + 1)
+    """Add two numbers in the log space."""
+    a, b = min(logx, logy), max(logx, logy)
+    if a == -np.inf:  # adding 0
+        return b
+    # Use exp(a) + exp(b) = (exp(a - b) + 1) * exp(b)
+    return math.log1p(math.exp(a - b)) + b  # log1p(x) = log(x + 1)
 
 
 def _compute_log_a_frac(q, sigma, alpha):
-  """Compute log(A_alpha) for fractional alpha. 0 < q < 1."""
-  # The two parts of A_alpha, integrals over (-inf,z0] and [z0, +inf), are
-  # initialized to 0 in the log space:
-  log_a0, log_a1 = -np.inf, -np.inf
-  i = 0
+    """Compute log(A_alpha) for fractional alpha. 0 < q < 1."""
+    # The two parts of A_alpha, integrals over (-inf,z0] and [z0, +inf), are
+    # initialized to 0 in the log space:
+    log_a0, log_a1 = -np.inf, -np.inf
+    i = 0
 
-  z0 = sigma**2 * math.log(1 / q - 1) + .5
+    z0 = sigma**2 * math.log(1 / q - 1) + .5
 
-  while True:  # do ... until loop
-    coef = special.binom(alpha, i)
-    log_coef = math.log(abs(coef))
-    j = alpha - i
+    while True:  # do ... until loop
+        coef = special.binom(alpha, i)
+        log_coef = math.log(abs(coef))
+        j = alpha - i
 
-    log_t0 = log_coef + i * math.log(q) + j * math.log(1 - q)
-    log_t1 = log_coef + j * math.log(q) + i * math.log(1 - q)
+        log_t0 = log_coef + i * math.log(q) + j * math.log(1 - q)
+        log_t1 = log_coef + j * math.log(q) + i * math.log(1 - q)
 
-    log_e0 = math.log(.5) + _log_erfc((i - z0) / (math.sqrt(2) * sigma))
-    log_e1 = math.log(.5) + _log_erfc((z0 - j) / (math.sqrt(2) * sigma))
+        log_e0 = math.log(.5) + _log_erfc((i - z0) / (math.sqrt(2) * sigma))
+        log_e1 = math.log(.5) + _log_erfc((z0 - j) / (math.sqrt(2) * sigma))
 
-    log_s0 = log_t0 + (i * i - i) / (2 * (sigma**2)) + log_e0
-    log_s1 = log_t1 + (j * j - j) / (2 * (sigma**2)) + log_e1
+        log_s0 = log_t0 + (i * i - i) / (2 * (sigma**2)) + log_e0
+        log_s1 = log_t1 + (j * j - j) / (2 * (sigma**2)) + log_e1
 
-    if coef > 0:
-      log_a0 = _log_add(log_a0, log_s0)
-      log_a1 = _log_add(log_a1, log_s1)
-    else:
-      log_a0 = _log_sub(log_a0, log_s0)
-      log_a1 = _log_sub(log_a1, log_s1)
+        if coef > 0:
+            log_a0 = _log_add(log_a0, log_s0)
+            log_a1 = _log_add(log_a1, log_s1)
+        else:
+            log_a0 = _log_sub(log_a0, log_s0)
+            log_a1 = _log_sub(log_a1, log_s1)
 
-    i += 1
-    if max(log_s0, log_s1) < -30:
-      break
+        i += 1
+        if max(log_s0, log_s1) < -30:
+            break
 
-  return _log_add(log_a0, log_a1)
+    return _log_add(log_a0, log_a1)
 
 
 def _log_erfc(x):
-  """Compute log(erfc(x)) with high accuracy for large x."""
-  try:
-    return math.log(2) + special.log_ndtr(-x * 2**.5)
-  except NameError:
-    # If log_ndtr is not available, approximate as follows:
-    r = special.erfc(x)
-    if r == 0.0:
-      # Using the Laurent series at infinity for the tail of the erfc function:
-      #     erfc(x) ~ exp(-x^2-.5/x^2+.625/x^4)/(x*pi^.5)
-      # To verify in Mathematica:
-      #     Series[Log[Erfc[x]] + Log[x] + Log[Pi]/2 + x^2, {x, Infinity, 6}]
-      return (-math.log(math.pi) / 2 - math.log(x) - x**2 - .5 * x**-2 +
-              .625 * x**-4 - 37. / 24. * x**-6 + 353. / 64. * x**-8)
-    else:
-      return math.log(r)
+    """Compute log(erfc(x)) with high accuracy for large x."""
+    try:
+        return math.log(2) + special.log_ndtr(-x * 2**.5)
+    except NameError:
+        # If log_ndtr is not available, approximate as follows:
+        r = special.erfc(x)
+        if r == 0.0:
+            # Using the Laurent series at infinity for the tail of the erfc function:
+            #     erfc(x) ~ exp(-x^2-.5/x^2+.625/x^4)/(x*pi^.5)
+            # To verify in Mathematica:
+            #     Series[Log[Erfc[x]] + Log[x] + Log[Pi]/2 + x^2, {x, Infinity, 6}]
+            return (-math.log(math.pi) / 2 - math.log(x) - x**2 - .5 * x**-2 +
+                    .625 * x**-4 - 37. / 24. * x**-6 + 353. / 64. * x**-8)
+        else:
+            return math.log(r)
 
 
 def _log_sub(logx, logy):
-  """Subtract two numbers in the log space. Answer must be non-negative."""
-  if logx < logy:
-    raise ValueError("The result of subtraction must be non-negative.")
-  if logy == -np.inf:  # subtracting 0
-    return logx
-  if logx == logy:
-    return -np.inf  # 0 is represented as -np.inf in the log space.
+    """Subtract two numbers in the log space. Answer must be non-negative."""
+    if logx < logy:
+        raise ValueError("The result of subtraction must be non-negative.")
+    if logy == -np.inf:  # subtracting 0
+        return logx
+    if logx == logy:
+        return -np.inf  # 0 is represented as -np.inf in the log space.
 
-  try:
-    # Use exp(x) - exp(y) = (exp(x - y) - 1) * exp(y).
-    return math.log(math.expm1(logx - logy)) + logy  # expm1(x) = exp(x) - 1
-  except OverflowError:
-    return logx
-
+    try:
+        # Use exp(x) - exp(y) = (exp(x - y) - 1) * exp(y).
+        return math.log(math.expm1(logx - logy)) + logy  # expm1(x) = exp(x) - 1
+    except OverflowError:
+        return logx
