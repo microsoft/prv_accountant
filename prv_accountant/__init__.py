e637ffb98ea142fcfb68f5d50f248e7a0cba14a5
Lukas Wutschitz <lukas.wutschitz@microsoft.com>
2021-11-03 18:33:00 +0000

    Fix merge conflicts

--- prv_accountant/__init__.py
+++ prv_accountant/__init__.py
@@ -1,9 +1,5 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-<<<<<<< HEAD
-from .accountant import Accountant  # noqa: F401
-=======
 from .accountant import Accountant, PRVAccountantHomogeneous, PRVAccountantHeterogenous, DPSGDAccountant  # noqa: F401
->>>>>>> luwutsch/composers
 from .privacy_random_variables import PrivacyRandomVariable  # noqa: F401
