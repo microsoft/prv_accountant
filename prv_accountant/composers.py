bc35935f492b0ed135d099ab8edd775dc841278a
Lukas Wutschitz <lukas.wutschitz@microsoft.com>
2021-11-11 19:38:59 +0000

    Prepare composer for hetero compo

--- prv_accountant/composers.py
+++ prv_accountant/composers.py
@@ -5,30 +5,39 @@ import numpy as np
 
 from abc import ABC, abstractmethod
 from scipy.fft import rfft, irfft
+from typing import Sequence
 
 from .discrete_privacy_random_variable import DiscretePrivacyRandomVariable
 
 
 class Composer(ABC):
     @abstractmethod
-    def compute_composition(self, num_compositions: int) -> DiscretePrivacyRandomVariable:
+    def compute_composition(self, num_compositions: Sequence[int]) -> DiscretePrivacyRandomVariable:
         pass
 
 
 class Fourier(Composer):
-    def __init__(self, prv: DiscretePrivacyRandomVariable) -> None:
+    def __init__(self, prvs: Sequence[DiscretePrivacyRandomVariable]) -> None:
         """
         Compute the composition of the PRVs using convolutions in Fourier space
 
         :param DiscretePrivacyRandomVariable prv: PRV to compose
         """
+        if len(prvs) != 1:
+            raise ValueError()
+
+        prv = prvs[0]
+
         if len(prv) % 2 != 0:
             raise ValueError("Can only compose evenly sized discrete PRVs")
 
         self.F = rfft(prv.pmf)
         self.domain = prv.domain
 
-    def compute_composition(self, num_compositions: int) -> DiscretePrivacyRandomVariable:
+    def compute_composition(self, num_compositions: Sequence[int]) -> DiscretePrivacyRandomVariable:
+        if len(num_compositions) != 1:
+            raise ValueError()
+
         f_n = irfft(self.F**num_compositions)
 
         m = num_compositions-1
