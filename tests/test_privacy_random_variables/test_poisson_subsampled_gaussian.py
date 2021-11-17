3422debf89599ebe91a9972a2d16e10b9eb85bbe
Lukas Wutschitz <lukas.wutschitz@microsoft.com>
2021-11-17 11:32:16 +0000

    Refactor tests

--- tests/test_privacy_random_variables/test_poisson_subsampled_gaussian.py
+++ tests/test_privacy_random_variables/test_poisson_subsampled_gaussian.py
@@ -87,17 +87,3 @@ class TestPoissonSubsampledGaussianMechanism:
         np.testing.assert_array_almost_equal(rdp, rdp_tf)
 
 
-class TestPrivacyRandomVariableTruncated:
-    def test_robustness(self):
-        p = 0.00011878424327013022
-        mu = 0.2
-
-        prv = PrivacyRandomVariableTruncated(
-            prv=PoissonSubsampledGaussianMechanism(
-                sampling_probability=p, noise_multiplier=mu
-            ),
-            t_min=-134.4230546183631,
-            t_max=134.42325451405242
-        )
-
-        assert prv.mean() > 1e-4
