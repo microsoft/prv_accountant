3115c1177391975adffe5548b37a124f967a9314
Lukas Wutschitz <lukas.wutschitz@microsoft.com>
2021-11-19 15:10:01 +0000

    Add laplace test

---
+++ tests/test_privacy_random_variables/test_laplace.py
@@ -0,0 +1,9 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+
+from prv_accountant.privacy_random_variables import LaplaceMechanism
+
+class TestLaplaceMechanism:
+    def test_normalised(self):
+        LaplaceMechanism()
