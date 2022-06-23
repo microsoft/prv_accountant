6d3b629bbbdc0ad2285b59013d4f6d15a5fad458
Lukas Wutschitz <luwutsch@microsoft.com>
2022-06-23 15:23:55 +0000

    Add tests for backwards compatibility

---
+++ tests/test_backwards_compatibility.py
@@ -0,0 +1,14 @@
+import pytest
+
+from prv_accountant import Accountant
+
+
+def test_accountant_warning():
+    with pytest.deprecated_call():
+        accountant = Accountant(
+            noise_multiplier=0.8,
+            sampling_probability=5e-3,
+            delta=1e-6,
+            eps_error=0.1,
+            max_compositions=1000
+        )
